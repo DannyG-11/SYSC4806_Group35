ef02f1ad6a70c90ad1c92874ec5d73e09189f5af
Colin Chen <colin1843@gmail.com>
2025-11-02 19:52:30 -0500

    remove greeting from controller

--- src/main/java/org/example/controllers/GraduateAdmissionsViewController.java
+++ src/main/java/org/example/controllers/GraduateAdmissionsViewController.java
@@ -14,12 +14,6 @@ public class GraduateAdmissionsViewController {
     public GraduateAdmissionsViewController(ApplicationFileRepository applicationFileRepository) {
         this.applicationFileRepository = applicationFileRepository;
     }
-  
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
 
     @GetMapping("/")
     public String home() {
